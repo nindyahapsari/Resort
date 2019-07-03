3bcb9a8881d01abe6dcf461392872b32dc12fd08
Nindya Hapsari <nindyahapsariiljas@yahoo.com>
2019-07-04 01:34:34 +0200

    update single room and featured rooms

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import defaultBcg from "../images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 export default class SingleRoom extends Component {
   render() {
     return <div>FROM SINGLE ROOM PAGE</div>;
   }
 }
+
+// video youtube 2:32:00

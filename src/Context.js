6753183eb86ffc477c7f406cd7bd31c4b31a5311
Nindya Hapsari <nindyahapsariiljas@yahoo.com>
2019-07-09 21:34:40 +0200

    adding filter

--- src/Context.js
+++ src/Context.js
@@ -8,7 +8,16 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: true
+    loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   };
 
   // getData
@@ -16,11 +25,17 @@ class RoomProvider extends Component {
   componentDidMount() {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
+
     this.setState({
       rooms,
       sortedRooms: rooms,
       featuredRooms,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
     });
   }
 
@@ -40,9 +55,80 @@ class RoomProvider extends Component {
     return room;
   };
 
+  handleChange = event => {
+    const target = event.target;
+    const name = event.target.name;
+    const value = event.type === "checkbox" ? target.checked : target.value;
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
+  };
+
+  filterRooms = () => {
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+
+    // all the rooms
+    let tempRooms = [...rooms];
+
+    // transform value
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+
+    // filter by type
+    if (type !== "all") {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+
+    //filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+    }
+
+    //Filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+
+    //Filter by SIZE
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+
+    //Filter by BREAKFAST
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+
+    // Filter by PETS
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
+
+    //change state
+    this.setState({
+      sortedRooms: tempRooms
+    });
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
@@ -50,5 +136,15 @@ class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <RoomConsumer>
+        {value => <Component {...props} context={value} />}
+      </RoomConsumer>
+    );
+  };
+}
 
 export { RoomContext, RoomProvider, RoomConsumer };
---
+++ src/components/RoomContainer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import RoomsFilter from "./RoomFilter";
+import RoomsList from "./RoomList";
+import { withRoomConsumer } from "../Context";
+import Loading from "./Loading";
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
+    </div>
+  );
+}
+
+export default withRoomConsumer(RoomContainer);
+
+// import React from "react";
+// import RoomsFilter from "./RoomFilter";
+// import RoomsList from "./RoomList";
+// import { RoomConsumer } from "../Context";
+// import Loading from "./Loading";
+
+// export default function RoomContainer() {
+//   return (
+//     <RoomConsumer>
+//       {value => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+
+//         return (
+//           <div>
+//             From Room Container
+//             <RoomsFilter />
+//             <RoomsList />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
